4662a56ea73070be2959ef5c22d810d472ac95c8
juru-juliette <kangabejuliette@gmail.com>
2019-09-10 12:53:00 +0200

    first commit

---
+++ app/views.py
@@ -0,0 +1,28 @@
+from flask import render_template
+from app import app
+
+# Views
+# @app.route('/')
+# def index():
+
+#     '''
+#     View root page function that returns the index page and its data
+#     '''
+#     message = 'Hello World'
+#     return render_template('index.html',message = message)
+#############dynamic routes
+@app.route('/movie/<movie_id>')
+def movie(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = movie_id)
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title)
