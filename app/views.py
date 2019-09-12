00c33cdd545e27756c5b4248652efaf5ba426127
juru-juliette <kangabejuliette@gmail.com>
2019-09-12 11:48:03 +0200

    searching movie through a list

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from flask import render_template
 from app import app
-from .request import get_movies
-from .requests import get_movies,get_movie
+from .request import get_movies,get_movie,search_movie
+from flask import render_template,request,redirect,url_for
+# from .request import get_movies,get_movie
 # Views
 # @app.route('/')
 # def index():
@@ -12,14 +13,14 @@ from .requests import get_movies,get_movie
 #     message = 'Hello World'
 #     return render_template('index.html',message = message)
 #############dynamic routes
-@app.route('/movie/<movie_id>')
-def movie(movie_id):
+# @app.route('/movie/<movie_id>')
+# def movie(movie_id):
 
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
+#     '''
+#     View movie page function that returns the movie details page and its data
+#     '''
     
-    return render_template('movie.html',id = movie_id)
+#     return render_template('movie.html',id = movie_id)
 
 @app.route('/movie/<int:id>')
 def movie(id):
@@ -31,7 +32,20 @@ def movie(id):
     title = f'{movie.title}'
 
     return render_template('movie.html',title = title,movie = movie)
-@app.route('/')   
+ 
+############################################
+## searching movie
+@app.route('/search/<movie_name>')
+def search(movie_name):
+    '''
+    View function to display the search results
+    '''
+    movie_name_list = movie_name.split(" ")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html',movies = searched_movies)
+@app.route('/')  
 def index():
 
     '''
@@ -43,4 +57,9 @@ def index():
     now_showing_movie = get_movies('now_playing')
     # print(popular_movies)
     title = 'Home - Welcome to The best Movie Review Website Online'
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name=search_movie))
+    else:
     return render_template('index.html', title = title,popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
